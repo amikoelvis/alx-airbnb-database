6ef7e4e4dc87f49b8dba07dc99ab315b79791280
Amiko Elvis <amikoelvis@gmail.com>
2025-07-02 20:38:24 +0300

    Add query performance before and after adding indexes using EXPLAIN

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -13,3 +13,17 @@ CREATE INDEX idx_bookings_dates ON bookings(start_date, end_date);
 CREATE INDEX idx_properties_host_id ON properties(host_id);
 CREATE INDEX idx_properties_location ON properties(location);
 CREATE INDEX idx_properties_price ON properties(price_per_night);
+
+-- Before Indexing
+EXPLAIN
+SELECT u.first_name, b.start_date
+FROM users u
+JOIN bookings b ON u.user_id = b.user_id
+WHERE b.start_date >= '2025-01-01';
+
+-- After Indexing
+EXPLAIN
+SELECT u.first_name, b.start_date
+FROM users u
+JOIN bookings b ON u.user_id = b.user_id
+WHERE b.start_date >= '2025-01-01';
